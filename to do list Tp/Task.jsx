65b5be452bb61a8bb270109bbf8d917e0a24e7b6
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2025-01-11 16:04:24 +0100

    implémentation de fonction d'ajout

---
+++ to do list Tp/Task.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Task(props) {
+
+ 
+      
+  }
+        return (
+          <div className='task'>
+              <h2 className='title'>Task : {props.task.tach}</h2>
+              <h2 className='title'>Description :{props.task.description} </h2>
+              <h2 className='title'>Date {props.task.date} : </h2>     
+                 <h2 className='title'>Catégorie :{props.task.catégoie} </h2>
+                 <div className="button">
+                 <button onClick={()=>alert('hey')}>Modifier ✍</button>
+                 <button onClick={()=>supprimer()}>Suppriner 🗑</button>
+                 </div>
+      
+      
+      
+      
+              
+          </div>
+        )
+      }
+export default Task;
