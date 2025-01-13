39271370c8d24537f067ec96cc8e04f7dbc0cba7
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2025-01-13 19:41:18 +0100

    Create Cards.jsx

---
+++ RoutingTP1/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Cards() {
+  return (
+<div className="cars"> 
+    <Card/>
+    <Card />
+    <Card />
+    <Card />
+  </div>
+      
+  )
+}
